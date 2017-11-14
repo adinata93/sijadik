a05c76ab12620a76182408d3a036e9fd13a46a6f
Brian Adi Kusumo <brian@nostratech.com>
2017-11-14 15:44:20 +0700

    Revisi
    revisi

--- config/web.php
+++ config/web.php
@@ -74,9 +74,7 @@ $config = [
 ];
 
 if (YII_ENV_DEV) {
-    // configuration adjustments for 'dev' environment
-<<<<<<< HEAD
-    
+    // configuration adjustments for 'dev' environment    
 	/*$config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
